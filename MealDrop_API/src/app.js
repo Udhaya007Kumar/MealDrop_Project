57905e5e42854e7ba3ff31d336842e8d5013c9ef
Udhaya007Kumar <udhaya93kumar@gmail.com>
2025-12-24 21:46:04 +0530

    Initial commit - payment service

--- MealDrop_API/src/app.js
+++ MealDrop_API/src/app.js
@@ -1,13 +1,23 @@
 import express from "express";
+import cors from "cors";
 import userRoutes from "./routes/userRoutes.js";
 import { errorHandler } from "./middleware/errorHandler.js";
 
 const app = express();
+
+/* ✅ CORS MUST COME FIRST */
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
+
+/* Handle preflight requests */
+app.options("*", cors());
 
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
 
 app.use(errorHandler);
-
+cd
 export default app;
